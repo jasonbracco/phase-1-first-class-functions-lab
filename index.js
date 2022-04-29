c5cd12337d0694b4457ed4c95cdb1feb71297051
Jason Bracco <bracco.jason@gmail.com>
2022-04-29 08:35:39 -0400

    commit tree 09ced0c26df9f0e3dd1a7c56b9eb69ca96f6a386

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return [drivers[0],drivers[1]]
+} 
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2) //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(number){
+    return function(fare){
+        return number*fare
+        }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(array, neededFunction){
+    return neededFunction(array)
+}
